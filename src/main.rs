dc2fc706fcae1d8a2474fffe1f6be8845393affc
Furkan Kalabalik <furkankalabalikk@gmail.com>
2022-11-23 23:49:36 +0300

    Conversion method changed from code generation to descriptor files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-extern crate prost_build;
-use std::env::temp_dir;
-use prost::Message;
+use std::{env::temp_dir, path::Path};
 use clap::{Parser, Arg};
+use protobuf::descriptor::FileDescriptorProto;
+use protobuf::reflect::FileDescriptor;
+use protobuf::reflect::ReflectValueBox;
+use std::fs;
 
 // Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -25,9 +27,25 @@ fn main() {
     let mut tmp = temp_dir();
     tmp.push(&args.message);
     std::fs::create_dir_all(&tmp).expect("Cannot create temp directory");
-    let mut prost_conf = prost_build::Config::new();
-    prost_conf.out_dir(&tmp)
-        .btree_map(&["."])
-        .compile_protos(&[&args.top_level_path], &[&args.include_path])
-        .expect("Couldn't compile proto files");
+    let top_level_path = Path::new(&args.top_level_path);
+    let include_path = Path::new(&args.include_path);
+
+    // Parse text `.proto` file to `FileDescriptorProto` message.
+    // Note this API is not stable and subject to change.
+    // But binary protos can always be generated manually with `protoc` command.
+    let mut file_descriptor_protos = protobuf_parse::Parser::new()
+        .pure()
+        .includes([&include_path])
+        .input(&top_level_path)
+        .parse_and_typecheck()
+        .unwrap()
+        .file_descriptors;
+
+    // // This is our .proto file converted to `FileDescriptorProto` from `descriptor.proto`.
+    // let mut file_descriptor_proto: Vec<&FileDescriptorProto> = file_descriptor_protos.iter()
+    //     .filter(|proto| proto.name() == top_level_path.file_name().unwrap().to_str().unwrap()).collect();
+    // let message_descriptor = file_descriptor_proto.pop().unwrap();
+    // message_descriptor.
+    
+    // println!("{}", message_descriptor.name());
 }
